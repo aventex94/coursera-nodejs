578ae57df2848dd21de5f2ec376379278eaad93c
Matias Miletich <matiasmiletich.informatica@gmail.com>
2020-09-08 18:18:53 -0300

    se agrego procedimiento del curso

---
+++ red-bicicletas/public/js/map.js
@@ -0,0 +1,5 @@
+var mymap = L.map('main_map').setView([-34.7066533,-58.3397637], 13);
+
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+}).addTo(mymap);
